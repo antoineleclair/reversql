2ac304c223f509b8f50801806e71e1783519b52d
Antoine Leclair <antoineleclair@gmail.com>
2012-07-19 10:50:13 -0400

    Uses re.IGNORECASE instead of brute forcing the regex.

--- reversql.py
+++ reversql.py
@@ -7,12 +7,12 @@ def print_reverse(filename):
             print 'DROP %s %s;' % create
 
 def creates(f):
-    regex = re.compile(r'(create|CREATE)'
+    regex = re.compile(r'CREATE'
                        r'\s+'
-                       r'(?P<table_or_sequence>table|TABLE|sequence|SEQUENCE)'
+                       r'(?P<table_or_sequence>TABLE|SEQUENCE)'
                        r'\s+'
                        r'(?P<name>\w+)'
-                       r'\s*\(?')
+                       r'\s*\(?', flags=re.IGNORECASE)
     for line in f:
         match = regex.match(line)
         if match is not None:
