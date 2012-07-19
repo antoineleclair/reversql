5add30c2f48f6c2dad0b21e00c40857b3c2fb2ab
Antoine Leclair <antoineleclair@gmail.com>
2012-07-19 10:05:05 -0400

    Adds script and updates readme.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-reversesql
-==========
+ReverSQL
+========
 
-Outputs the corresponding DROP of a SQL file for each CREATE TABLE and CREATE SEQUENCE, in reverse of their creation order.
+This is a simple script to take all the `CREATE TABLE` and `CREATE SEQUENCE` from a SQL file and output the corresponding `DROP` in reverse. I wrote this when dealing with Play Framework 2.0 evolutions (migrations) and Oracle database. I use it to generate the `down` evolution from a `up` evolution.
---
+++ reversql.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+import sys, os, re
+
+def print_reverse(filename):
+    with open(filename) as f:
+        for create in reversed(list(creates(f))):
+            print 'DROP %s %s;' % create
+
+def creates(f):
+    regex = re.compile(r'(create|CREATE)'
+                       r'\s+'
+                       r'(?P<table_or_sequence>table|TABLE|sequence|SEQUENCE)'
+                       r'\s+'
+                       r'(?P<name>\w+)'
+                       r'\s*\(?')
+    for line in f:
+        match = regex.match(line)
+        if match is not None:
+            yield match.group('table_or_sequence').upper(), match.group('name')
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit('Usage: %s filename' % sys.argv[0])
+    filename = sys.argv[1]
+    try:
+        with open(filename) as f: pass
+    except IOError as e:
+        sys.exit('File %s does not exist.' % sys.argv[0])
+    print_reverse(filename)
